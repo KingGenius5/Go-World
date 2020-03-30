dab3f306ec5060861e117a14c84e06664c4821a1
Muhammad Tifak <moht20009@gmail.com>
2020-03-30 04:08:54 -0700

    Importing multiple packages in Go

--- main.go
+++ main.go
@@ -3,9 +3,23 @@
 //In this case, we're creating an executable named main.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	t "time"
+)
+
+/*
+
+import (
+	You can import multiple packages using parentheses and refer to them using an alias.
+	p1 "package1"
+	p2 "package2"
+)
+
+*/
 
 //func is a function declaration in Go
 func main() {
 	fmt.Println("Hello Go World!")
+	fmt.Println(t.Now())
 }
