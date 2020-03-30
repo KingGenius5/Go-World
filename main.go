d06d9e7a6570ee35d8d0e61cdc8c6e8336c47818
Muhammad Tifak <moht20009@gmail.com>
2020-03-30 03:58:02 -0700

    Added comments to explain code and guide myself

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
+//This is called a package declaration.
+//It informs the compiler whether to create an executable or a library.
+//In this case, we're creating an executable named main.
 package main
 
 import "fmt"
 
+//func is a function declaration in Go
 func main() {
 	fmt.Println("Hello Go World!")
 }
